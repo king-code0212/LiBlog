17a8118c58233f77bf16372105e82e423531ad70
king-code0212 <liedwin463@gmail.com>
2020-10-18 23:58:42 +0800

    xo的utils

---
+++ li_blog_xo/src/main/java/com/steel/li_blog_xo/utils/WebUtil.java
@@ -0,0 +1,14 @@
+package com.steel.li_blog_xo.utils;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+
+/**
+ * @Auther: steel
+ * @Date: 2020/10/18 23:01
+ * @Description: web有关的工具类
+ */
+@Slf4j
+@Component
+public class WebUtil {
+}
