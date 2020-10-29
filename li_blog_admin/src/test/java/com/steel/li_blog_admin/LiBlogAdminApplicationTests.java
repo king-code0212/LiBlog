70a0ae95d1de492cf1802f30bd66aa12fee37fbc
ligang <liedwin463@gmail.com>
2020-10-29 12:40:32 +0800

    管理员登录

---
+++ li_blog_admin/src/test/java/com/steel/li_blog_admin/LiBlogAdminApplicationTests.java
@@ -0,0 +1,13 @@
+package com.steel.li_blog_admin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LiBlogAdminApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
